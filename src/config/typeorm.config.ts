dc82fb471e3548ef2cbe767e950983229691bca3
famesensor <sathit.ju@outlook.com>
2021-04-21 00:14:17 +0700

    [:tada:]: init comit config db

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,5 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
+export const typeOrmConfig: TypeOrmModuleOptions = {};
