14c5ecd31b68b97eced519a3ee2682915f85a24d
famesensor <sathit.ju@outlook.com>
2021-04-23 22:36:21 +0700

    [:hammer:]: add function validate user password

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,9 +1,21 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { SignCredentialsDto } from './dto/sign-credentials.dto';
 import { StudentInfo } from './model/student.entity';
 import { User } from './model/user.entity';
 
 @EntityRepository(User)
 @EntityRepository(StudentInfo)
 export class UserRepository extends Repository<User | StudentInfo> {
-  // async signIn()
+  async validateUserPassword(
+    signCredentialsDto: SignCredentialsDto,
+  ): Promise<User> {
+    const { username, password } = signCredentialsDto;
+    const user = (await this.findOne({ username })) as User;
+
+    if (user && (await user.validatePassword(password))) {
+      return user;
+    } else {
+      return null;
+    }
+  }
 }
