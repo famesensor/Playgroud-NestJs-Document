ac9fab2c64b4e92487bfbd99bedb74f911100c17
famesensor <sathit.ju@outlook.com>
2021-04-23 22:34:52 +0700

    [:hammer:] setup confog port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
+  const port = process.env.PORT || serverConfig.port;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
