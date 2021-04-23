85e44e22f0bf0972d5155e7bbe8ced4a9e6862fc
famesensor <sathit.ju@outlook.com>
2021-04-23 22:35:38 +0700

    [:hammer:]: setup service auth

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AuthenticationController } from './authentication.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from 'src/user/user.repository';
 import { AuthenticationService } from './authentication.service';
+import {
+  JWTExportationModules,
+  model_importation,
+} from './strategies/jwt.strategy';
 
 @Module({
-  controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  imports: [...model_importation, TypeOrmModule.forFeature([UserRepository])],
+  providers: [AuthenticationService, JwtModule],
+  exports: [...JWTExportationModules],
 })
 export class AuthenticationModule {}
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SignCredentialsDto } from 'src/user/dto/sign-credentials.dto';
+import { UserRepository } from 'src/user/user.repository';
+import { JwtPayload } from './strategies/jwt.payload';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    signCredentialsDto: SignCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const user = await this.userRepository.validateUserPassword(
+      signCredentialsDto,
+    );
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials!');
+    }
+
+    const payload: JwtPayload = {
+      username: user.username,
+      role: user.role,
+      email: user.email,
+    };
+
+    const accessToken = this.jwtService.sign(payload);
+    return { accessToken };
+  }
+}
