9a5a73432a1e615f18855313c7c0f03ef5bca0d4
famesensor <sathit.ju@outlook.com>
2021-04-26 01:03:08 +0700

    [:hammer:]: delete type

--- src/authentication/strategies/jwt.strategy.ts
+++ src/authentication/strategies/jwt.strategy.ts
@@ -35,7 +35,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: JwtPayload): Promise<User> {
     const { username } = payload;
-    const user = (await this.userRepository.findOne({ username })) as User;
+    const user = await this.userRepository.findOne({ username });
 
     if (!user) {
       throw new UnauthorizedException();
