ad2baea78fefee211435be35f045f72f49c2f272
famesensor <sathit.ju@outlook.com>
2021-04-26 01:04:04 +0700

    [:hammer:]: add controller user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthenticationService } from 'src/authentication/authentication.service';
+import { StudentDto } from './dto/create-student.dto';
+import { TeacherDto } from './dto/create-teacher.dto';
+import { SignCredentialsDto } from './dto/sign-credentials.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private authService: AuthenticationService,
+    private userService: UserService,
+  ) {}
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) signCredentialsDto: SignCredentialsDto,
+  ): Promise<any> {
+    return this.authService.signIn(signCredentialsDto);
+  }
+
+  @Post('/signup-teacher')
+  signUpTeacher(@Body() teacherDto: TeacherDto): Promise<any> {
+    return this.userService.signUpTeacher(teacherDto);
+  }
+
+  @Post('/signup-student')
+  signUpStudent(@Body() studentDto: StudentDto): Promise<any> {
+    return this.userService.signUpStudent(studentDto);
+  }
+
+  @Get('/:id')
+  getUser(@Param('id') id: string): Promise<any> {
+    return this.userService.getStudentProfile(id);
+  }
+}
