4f1da90874a4d1f23d4607bea68315e13d80afba
famesensor <sathit.ju@outlook.com>
2021-04-26 01:05:02 +0700

    [:hammer:]: add service user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StudentDto } from './dto/create-student.dto';
+import { TeacherDto } from './dto/create-teacher.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+
+  async signUpTeacher(teacherDto: TeacherDto) {
+    return this.userRepository.signInTecher(teacherDto);
+  }
+
+  async signUpStudent(studentDto: StudentDto) {
+    return this.userRepository.signInStudent(studentDto);
+  }
+
+  async getStudentProfile(id: string) {
+    const data = await this.userRepository
+      .createQueryBuilder('user')
+      .where(`user.id = :id`, { id: id })
+      .leftJoinAndSelect('user.studentInfo', 'studentInfo')
+      .getMany();
+
+    return data;
+  }
+}
