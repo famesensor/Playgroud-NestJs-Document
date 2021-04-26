41b73206f79c8361a89ddcd337f4b7ff95627549
famesensor <sathit.ju@outlook.com>
2021-04-26 22:14:59 +0700

    [:hammer:]: fix provider

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/user/user.repository';
 import { AuthenticationService } from './authentication.service';
 import {
   JWTExportationModules,
+  JwtStrategy,
   model_importation,
 } from './strategies/jwt.strategy';
 
 @Module({
   imports: [...model_importation, TypeOrmModule.forFeature([UserRepository])],
-  providers: [AuthenticationService, JwtModule],
+  providers: [AuthenticationService, JwtStrategy],
   exports: [...JWTExportationModules],
 })
 export class AuthenticationModule {}
