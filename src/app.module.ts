db9f29f4466dd3ce99739f984de709961d448365
famesensor <sathit.ju@outlook.com>
2021-04-28 00:54:27 +0700

    [:hammer:]: add module document

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
+import { DocumentorModule } from './documentor/documentor.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     AuthenticationModule,
     UserModule,
+    DocumentorModule,
   ],
 })
 export class AppModule {}
