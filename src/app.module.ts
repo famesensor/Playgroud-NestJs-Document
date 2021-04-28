a5ac6b4c28653f39b2cdbd762afbbe3616ce70e0
famesensor <sathit.ju@outlook.com>
2021-04-29 01:00:41 +0700

    [:hammer:]: add mocule trasaction

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
-import { DocumentorModule } from './documentor/documentor.module';
+import { TrasactionModule } from './trasaction/trasaction.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     AuthenticationModule,
     UserModule,
-    DocumentorModule,
+    TrasactionModule,
   ],
 })
 export class AppModule {}
