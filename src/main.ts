332798feb1c3d3e20c4cb12699284ad321f55603
famesensor <sathit.ju@outlook.com>
2021-05-01 02:36:19 +0700

    [:hammer:]: add http execption

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { HttpExceptionFilter } from './commom/filters/exceptions.filter';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port);
 }
 bootstrap();
