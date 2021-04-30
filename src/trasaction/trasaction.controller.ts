78947d3fd15fb5777aa788f78dcc9130b8c84059
famesensor <sathit.ju@outlook.com>
2021-05-01 02:37:29 +0700

    [:hammer:]: setup route ro01

--- src/trasaction/trasaction.controller.ts
+++ src/trasaction/trasaction.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/shared/decorators/get-user.decorator';
+import { Role } from 'src/shared/enums/role.enum';
+import { RolesGuard } from 'src/shared/guards/role/role.guard';
+import { Roles } from 'src/shared/guards/role/roles.decorator';
+import { User } from 'src/user/entity/user.entity';
+import { RO01Dto } from './dto/create-ro01.dto';
+import { TrasactionService } from './trasaction.service';
 
 @Controller('trasaction')
-export class TrasactionController {}
+@UseGuards(AuthGuard(), RolesGuard)
+@Roles(Role.Student)
+export class TrasactionController {
+  constructor(private trasactionService: TrasactionService) {}
+
+  @Post('/create-ro01')
+  createRO01(@Body() ro01Dto: RO01Dto, @GetUser() user: User) {
+    return this.trasactionService.createRO01(user, ro01Dto);
+  }
+}
