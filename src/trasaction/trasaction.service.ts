ec26db0301cb960e849bb71c9cec390f0a81cf85
famesensor <sathit.ju@outlook.com>
2021-05-01 02:37:41 +0700

    [:hammer:]: setup service ro01

--- src/trasaction/trasaction.service.ts
+++ src/trasaction/trasaction.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entity/user.entity';
+import { UserRepository } from 'src/user/user.repository';
+import { Repository } from 'typeorm';
+import { RO01Repository } from './document-ro01.repository';
+import { RO01Dto } from './dto/create-ro01.dto';
+import { DocumentType } from './entity/document-type.entity';
 
 @Injectable()
-export class TrasactionService {}
+export class TrasactionService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+    @InjectRepository(DocumentType)
+    private documentType: Repository<DocumentType>,
+    @InjectRepository(RO01Repository) private ro01Repository: RO01Repository,
+  ) {}
+
+  async createRO01(user: User, ro01Dto: RO01Dto): Promise<any> {
+    const info = await this.userRepository.getUserDetail(user.id);
+
+    if (!info) {
+      throw new NotFoundException();
+    }
+
+    const typeInfo = await this.documentType.findOne({
+      type_name: 'RO-01 คำร้องทั่วไป',
+    });
+
+    if (!typeInfo) {
+      throw new InternalServerErrorException();
+    }
+
+    return this.ro01Repository.createDocumentRO01(ro01Dto, info, typeInfo);
+  }
+}
