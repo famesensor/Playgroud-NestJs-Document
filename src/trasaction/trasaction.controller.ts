0cb4dbeb6b0b605aafbd5b486370ce0a3d21579b
famesensor <sathit.ju@outlook.com>
2021-05-01 18:49:31 +0700

    [:hamme:]: init route create ro16

--- src/trasaction/trasaction.controller.ts
+++ src/trasaction/trasaction.controller.ts
@@ -12,6 +12,7 @@ import { RolesGuard } from 'src/shared/guards/role/role.guard';
 import { Roles } from 'src/shared/guards/role/roles.decorator';
 import { User } from 'src/user/entity/user.entity';
 import { RO01Dto } from './dto/create-ro01.dto';
+import { RO16Dto } from './dto/create-ro16.dto';
 import { TrasactionService } from './trasaction.service';
 
 @Controller('trasaction')
@@ -23,5 +24,10 @@ export class TrasactionController {
   @Post('/create-ro01')
   createRO01(@Body() ro01Dto: RO01Dto, @GetUser() user: User) {
     return this.trasactionService.createRO01(user, ro01Dto);
+  }
+
+  @Post('/create-ro16')
+  createRO16(@Body() ro16Dto: RO16Dto, @GetUser() user: User) {
+    return this.trasactionService.createRO16(user, ro16Dto);
   }
 }
