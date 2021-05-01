fade604cbee564c0411f4d72821d2487e0cf94ed
famesensor <sathit.ju@outlook.com>
2021-05-01 22:08:29 +0700

    [:hammer:]: setup route create 26 and setup role guard

--- src/trasaction/trasaction.controller.ts
+++ src/trasaction/trasaction.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  ParseArrayPipe,
   Post,
   UseGuards,
   ValidationPipe,
@@ -13,21 +14,29 @@ import { Roles } from 'src/shared/guards/role/roles.decorator';
 import { User } from 'src/user/entity/user.entity';
 import { RO01Dto } from './dto/create-ro01.dto';
 import { RO16Dto } from './dto/create-ro16.dto';
+import { RO26Dto } from './dto/create-ro26.dto';
 import { TrasactionService } from './trasaction.service';
 
 @Controller('trasaction')
 @UseGuards(AuthGuard(), RolesGuard)
-@Roles(Role.Student)
 export class TrasactionController {
   constructor(private trasactionService: TrasactionService) {}
 
+  @Roles(Role.Student)
   @Post('/create-ro01')
   createRO01(@Body() ro01Dto: RO01Dto, @GetUser() user: User) {
     return this.trasactionService.createRO01(user, ro01Dto);
   }
 
+  @Roles(Role.Student)
   @Post('/create-ro16')
   createRO16(@Body() ro16Dto: RO16Dto, @GetUser() user: User) {
     return this.trasactionService.createRO16(user, ro16Dto);
+  }
+
+  @Roles(Role.Student)
+  @Post('/create-ro26')
+  createRO26(@Body() ro26Dto: RO26Dto, @GetUser() user: User) {
+    return this.trasactionService.createRO26(user, ro26Dto.subject);
   }
 }
