cec8bd5b2e39fd9f668941d19cf8a619479203d9
famesensor <sathit.ju@outlook.com>
2021-05-01 18:50:21 +0700

    [:hammer:]: add ro16 repo

--- src/trasaction/trasaction.module.ts
+++ src/trasaction/trasaction.module.ts
@@ -4,13 +4,19 @@ import { AuthenticationModule } from 'src/authentication/authentication.module';
 import { UserRepository } from 'src/user/user.repository';
 import { UserService } from 'src/user/user.service';
 import { RO01Repository } from './document-ro01.repository';
+import { RO16Repository } from './document-ro16.repository';
 import { DocumentType } from './entity/document-type.entity';
 import { TrasactionController } from './trasaction.controller';
 import { TrasactionService } from './trasaction.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RO01Repository, DocumentType, UserRepository]),
+    TypeOrmModule.forFeature([
+      RO01Repository,
+      RO16Repository,
+      DocumentType,
+      UserRepository,
+    ]),
     AuthenticationModule,
   ],
   providers: [TrasactionService, UserService],
