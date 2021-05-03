05a79e1e8103f74e347444994e84737b2be7c7ec
famesensor <sathit.ju@outlook.com>
2021-05-04 00:05:14 +0700

    [:hammer:]: fix error st time

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,4 @@
 export const addMinutes = (date: Date, minutes: number): Date => {
-  return new Date(date.getMinutes() + minutes);
+  const time = new Date().setMinutes(date.getMinutes() + minutes);
+  return new Date(time);
 };
