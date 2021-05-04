00ca14de4342d31d11c839d01ab9e5b96cfe804c
famesensor <sathit.ju@outlook.com>
2021-05-05 01:23:37 +0700

    [:hammer:]: fix approve interface

--- src/interfaces/IPdfOption.ts
+++ src/interfaces/IPdfOption.ts
@@ -1,3 +1,4 @@
+import { Approve } from 'src/trasaction/entity/approve.entity';
 import { DocumentRO01 } from 'src/trasaction/entity/document-ro01.entity';
 import { DocumentRO16 } from 'src/trasaction/entity/document-ro16.entity';
 import { DocumentRO26 } from 'src/trasaction/entity/document-ro26.entity';
@@ -6,6 +7,6 @@ import { User } from 'src/user/entity/user.entity';
 export interface IPdfOption {
   template: string;
   student: User;
-  teachers: User[];
+  approves: Approve[];
   data: DocumentRO01 | DocumentRO16 | DocumentRO26;
 }
