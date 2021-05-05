bfca9e1897614ecd311d123cdb987a0cb89f1108
famesensor <sathit.ju@outlook.com>
2021-05-05 23:39:17 +0700

    :recycle: refactor authen

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -2,8 +2,10 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from 'src/shared/enums/role.enum';
-import { SignCredentialsDto } from 'src/user/dto/sign-credentials.dto';
 import { UserRepository } from 'src/user/user.repository';
+import { StudentDto } from './dto/create-student.dto';
+import { TeacherDto } from './dto/create-teacher.dto';
+import { SignInCredentialsDto } from './dto/signIn-credentials.dto';
 import { JwtPayload } from './strategies/jwt.payload';
 
 @Injectable()
@@ -15,10 +17,10 @@ export class AuthenticationService {
   ) {}
 
   async signIn(
-    signCredentialsDto: SignCredentialsDto,
+    signInCredentialsDto: SignInCredentialsDto,
   ): Promise<{ accessToken: string }> {
     const user = await this.userRepository.validateUserPassword(
-      signCredentialsDto,
+      signInCredentialsDto,
     );
     if (!user) {
       throw new UnauthorizedException('Invalid credentials!');
@@ -36,5 +38,13 @@ export class AuthenticationService {
 
     const accessToken = this.jwtService.sign(payload);
     return { accessToken };
+  }
+
+  async signUpTeacher(teacherDto: TeacherDto) {
+    return this.userRepository.signInTecher(teacherDto);
+  }
+
+  async signUpStudent(studentDto: StudentDto) {
+    return this.userRepository.signInStudent(studentDto);
   }
 }
