1372f58e751f80d64281c59b86ea1567854f0e8d
famesensor <sathit.ju@outlook.com>
2021-05-23 01:57:42 +0700

    [:fire:]: delete useless

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,16 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { TrasactionModule } from './transaction/transaction.module';
-import { ConfigModule } from '@nestjs/config';
-import { LoggerMiddleware } from './commom/middlewares/logger.middleware';
-import { AuthenticationController } from './authentication/authentication.controller';
-import { UserController } from './user/user.controller';
-import { TrasactionController } from './transaction/transaction.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot(typeOrmConfig),
     AuthenticationModule,
     UserModule,
     TrasactionModule,
   ],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes(
-        AuthenticationController,
-        UserController,
-        TrasactionController,
-      );
-  }
-}
+export class AppModule {}
