96071d5a73135397ad8ff7bd511c788285115b0e
famesensor <sathit.ju@outlook.com>
2021-05-26 00:50:36 +0700

    [:wrench:]: update config email

--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -3,15 +3,17 @@ import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
 
 export const mailConfig: MailerOptions = {
   transport: {
-    host: process.env.HOST,
+    host: process.env.HOST_EMAIL,
     secure: true,
     auth: {
-      user: process.env.USER,
-      pass: process.env.PASS,
+      type: process.env.TYPE_EMAIL,
+      user: process.env.USER_EMAIL,
+      pass: process.env.PASS_EMAIL,
     },
+    port: process.env.PORT_EMAIL_EMAIL,
   },
   defaults: {
-    from: process.env.FROM,
+    from: process.env.FROM_EMAIL,
   },
   template: {
     adapter: new EjsAdapter(), // or new PugAdapter() or new EjsAdapter()
